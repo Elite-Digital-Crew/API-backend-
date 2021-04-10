ce00a6298792b42f6401c62c041a3641475da682
TejaswiniAnbazhagan <76993979+TejaswiniAnbazhagan@users.noreply.github.com>
2021-04-10 16:45:15 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -57,21 +57,61 @@ app.get('/', (req, res) => {
 //@route POST /upload
 //@desc uploads file to db
 app.post('/upload', express.static('upload'), upload.single('file') ,(req, res)=>{
-    res.json({file: req.file})
-    //res.redirect('/')
+    //res.json({file: req.file})
+    res.redirect('/')
 })
 //route GET files
 // desc: displays all files in json
 
 app.get('/files', (req, res) => {
   gfs.files.find().toArray((err, files) => {
-    if(!files) {
+    if(!files || files.length === 0) {
       return res.status(404).json({
         err: 'no files exist'})
     }
   return res.json(files)
   })
 })
+
+
+//route GET /files/:filename
+// desc: displays single file obj
+app.get('/files/:filename', (req, res) => {
+  gfs.files.findOne({filename: req.params.filename}, (err, file) =>{
+    if(!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'no file exist'})
+    }
+  return res.json(file)
+  })
+
+})
+
+
+//@route GET /image/:filename
+//@desc Display image
+app.get('/image/:filename', (req, res) => {
+  gfs.files.findOne({filename: req.params.filename}, (err, file) =>{
+    if(!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'no such file exists'
+      })
+    }
+
+    //check if image
+    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+      //read output to browser
+      const readstream = gfs.createReadStream(file.filename)
+      readstream.pipe(res)
+    } else {
+      res.status(404).json({
+        err: "not an image"
+      })
+    }
+  })
+
+})
+
 
 const port = 5000
 app.listen(port, () =>{
